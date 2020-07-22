5d4f319f17037b98366eb5fec81416306666e364
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-07-22 22:23:09 +0530

    Add files via upload

---
+++ 4_area_of_circle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 02:03:30 2020
+
+@author: trendydice
+"""
+
+#Write a Python program which accepts the radius of a circle 
+#from the user and compute the area.
+
+import math
+radius = input("enter radius")
+area = math.pi*float(radius)**2
+print('area is', area)
+
